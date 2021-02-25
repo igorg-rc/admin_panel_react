c401be0647f3732cc53db379dbae39258191d764
igorg-rc <igor.gayvoronsky.rc@gmail.com>
2021-02-25 09:36:31 +0200

    hide scrollbar on login and messages pages

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -4,60 +4,60 @@ import logo from '../../img/layout/index1_square_white_96.png'
 
 export default function Login () {
   return (
-    <div id="login" style={{ paddingTop: '75px', paddingLeft: '' }}>
-      <div className="" style={{ height: '100vh' }}>
+    <div id="page">
+      <div id="login" style={{ paddingTop: '75px', paddingLeft: '' }}>
+        <div className="" style={{ height: '100vh' }}>
 
-        <div class="row">
-          <div className="col s6 offset-s3 form-wrapper indigo darken-1 white-text">
-            <div className="row">
-              <div className="col s12 logo-handler">
-                <img src={logo} alt="logo" className="form_logo-img" />
-                <h3>BonfireApps</h3>
-                <h6>Admin panel</h6>
-              </div>
-            </div>
-
-
-            <form class="">
+          <div class="row">
+            <div className="col s6 offset-s3 form-wrapper indigo darken-1 white-text">
               <div className="row">
-                <div class="input-field col s10 offset-s1">
-                  <i class="material-icons prefix">account_circle</i>
-                  <input id="username" type="text" name="username" />
-                  <label for="username">Login</label>
+                <div className="col s12 logo-handler">
+                  <img src={logo} alt="logo" className="form_logo-img" />
+                  <h3>BonfireApps</h3>
+                  <h6>Admin panel</h6>
                 </div>
               </div>
 
 
-              <div className="row">
-                <div class="input-field col s10 offset-s1">
-                  <i class="material-icons prefix">vpn_key</i>
-                  <input id="password" type="password" name="password" />
-                  <label for="password">Password</label>
-                </div>
-              </div>
-
-            
-              <div className="form-links">
+              <form class="">
                 <div className="row">
-                  <div className="col s6 left">
-                    
+                  <div class="input-field col s10 offset-s1">
+                    <i class="material-icons prefix">account_circle</i>
+                    <input id="username" type="text" name="username" />
+                    <label for="username">Login</label>
                   </div>
                 </div>
-              </div>
 
-              <div className="row">
-                <div class="input-field col s10 offset-s1">
-                  <a class="waves-effect waves-light btn"><i class="material-icons left">login</i>Sign in</a>
+
+                <div className="row">
+                  <div class="input-field col s10 offset-s1">
+                    <i class="material-icons prefix">vpn_key</i>
+                    <input id="password" type="password" name="password" />
+                    <label for="password">Password</label>
+                  </div>
                 </div>
-              </div>
 
-            </form>
+              
+                <div className="form-links">
+                  <div className="row">
+                    <div className="col s6 left">
+                      
+                    </div>
+                  </div>
+                </div>
+
+                <div className="row">
+                  <div class="input-field col s10 offset-s1">
+                    <a class="waves-effect waves-light btn"><i class="material-icons left">login</i>Sign in</a>
+                  </div>
+                </div>
+
+              </form>
+            </div>
           </div>
         </div>
+        
       </div>
-
-      
-
     </div>
   )
 }
--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -5,15 +5,15 @@ import Layout from '../../Layout/Layout'
 export default function Home() {
 	return (
 		<>
-		<Layout />
-		<div>
-			<h1>Home component!</h1>
-			<div className="col s12 blue-grey lighten-4">
-				<div className="content">
-					
+			<Layout />
+			<div>
+				<h1>Home component!</h1>
+				<div className="col s12 blue-grey lighten-4">
+					<div className="content">
+						
+					</div>
 				</div>
 			</div>
-		</div>
 		</>
 	)
 }
